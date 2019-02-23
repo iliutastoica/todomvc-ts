541863071a7a18c2b84bfa8fdd4275ef4175855d
Cristi <ratoi_crysty@yahoo.com>
2019-02-23 09:19:39 +0200

    Add minor improvements

--- src/constants/TodoFilters.ts
+++ src/constants/TodoFilters.ts
@@ -3,7 +3,3 @@ export enum TodoFilter {
   ShowCompleted = 'show_completed',
   ShowActive = 'show_active',
 }
-
-export const SHOW_ALL = 'show_all'
-export const SHOW_COMPLETED = 'show_completed'
-export const SHOW_ACTIVE = 'show_active'
